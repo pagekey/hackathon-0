ce08267850ea7ca06fcd5dd9bd2740921ce3fd58
Jake Cyr <cyrjake@gmail.com>
2024-03-09 16:08:19 -0500

    Add account list item

--- src/components/AccountList.tsx
+++ src/components/AccountList.tsx
@@ -1,5 +1,6 @@
 import { Account } from '@/models/Account';
-import { Stack, Flex } from '@mantine/core';
+import { Stack } from '@mantine/core';
+import { AccountListItem } from './AccountListItem';
 
 export interface AccountListProps {
   accounts: Account[];
@@ -9,22 +10,7 @@ export const AccountList: React.FC<AccountListProps> = ({ accounts }) => {
   return (
     <Stack>
       {accounts.map((account: Account) => (
-        <Flex key={account.username}>
-          <div className='px-2'>
-            <img
-              src={
-                account.provider === 'github'
-                  ? `https://github.com/${account.username}.png`
-                  : '/gitlab.png'
-              }
-              alt={account.username}
-              height={32}
-              width={32}
-            />
-          </div>
-          <div>{account.username}</div>
-          <div>{account.provider}</div>
-        </Flex>
+        <AccountListItem key={account.provider + account.username} account={account} />
       ))}
     </Stack>
   );
