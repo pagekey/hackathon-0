27c08e1b3eefb1d291e264447199626a733ffed1
Jake Cyr <cyrjake@gmail.com>
2024-03-09 12:03:31 -0500

    Update routes and load env

--- api/email_agent_api/main.py
+++ api/email_agent_api/main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 
-@app.get("/api/generate")
+@app.post("/api/generate")
 def generate_email() -> dict[str, str]:
-    return {"Hello": "World"}
+    return {"email": "Hey Mr. Smith, here is your receipt"}
 
 
-@app.get("/api/register")
+@app.post("/api/register")
 def register_user() -> dict[str, str]:
-    return {"Hello": "World"}
+    return {"token": "some-api-token"}
 
 
 @app.get("/api/user")
 def get_user_status() -> dict[str, str]:
-    return {"Hello": "World"}
+    return {"status": "PAID"}
