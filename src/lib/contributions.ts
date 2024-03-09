f942dbe8792c38647bd38a9887ac8964c2bbd88f
Jake Cyr <cyrjake@gmail.com>
2024-03-09 17:15:25 -0500

    Increase performance of github contribution parser

--- src/lib/contributions.ts
+++ src/lib/contributions.ts
@@ -13,50 +13,59 @@ export async function getContributions(account: Account): Promise<Contribution[]
 }
 
 async function getGithubContributions(username: string): Promise<Contribution[]> {
-    try {
-        const response = await fetch(`https://github.com/${username}`);
-        if (response.status == 200) {
-            // Parse DOM returned in HTML response
-            const dom = parseFromString(await response.text());
-            const contributions = dom.getElementsByClassName('ContributionCalendar-day');
-            const result =  contributions.map(contribution => {
-                const date = contribution.getAttribute('data-date');
-                const tooltips = dom.getElementsByAttribute('for', contribution.getAttribute('id'));
-                if (tooltips[0] && tooltips[0].innerHTML) {
-                    const count = parseInt(tooltips[0].innerHTML.split(' ')[0]) || 0;
-                    const result: Contribution = { date, count };
-                    console.log('got small result',result)
-                    return result;
-                } else {
-                    return { date, count: 0 };
-                }
-            }).filter(contribution => contribution.count > 0);
-            console.log('got big result',result)
-            return result;
-        } else {
-            return [];
+  try {
+    const response = await fetch(`https://github.com/${username}`);
+    if (response.status == 200) {
+      // Parse DOM returned in HTML response
+      const dom = parseFromString(await response.text());
+      const contributions = dom.getElementsByClassName('ContributionCalendar-day');
+      const result = contributions.map(async (contribution) => {
+        const date = contribution.getAttribute('data-date');
+        const level = parseInt(contribution.getAttribute('data-level'));
+
+        if (level === 0) {
+          return { date, count: 0 };
         }
-    } catch (error) {
-        console.error(error);
-        return [];
+
+        const tooltips = dom.getElementsByAttribute('for', contribution.getAttribute('id'));
+        if (tooltips[0] && tooltips[0].innerHTML) {
+          const count = parseInt(tooltips[0].innerHTML.split(' ')[0]) || 0;
+          const result: Contribution = { date, count };
+          console.log('got small result', result);
+          return result;
+        } else {
+          return { date, count: 0 };
+        }
+      });
+
+      const responses = await Promise.all(result);
+      const filteredResponses = responses.filter((contribution) => contribution.count > 0);
+      console.log('got big result', filteredResponses);
+      return filteredResponses;
+    } else {
+      return [];
     }
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 async function getGitlabContributions(username: string): Promise<Contribution[]> {
-    try {
-        const response = await fetch(`https://gitlab.com/users/${username}/calendar.json`);
-        if (response.status == 200) {
-            const data: Map<string, string> = await response.json();
-            const result = Object.entries(data).map(([date, count]) => ({
-                date,
-                count,
-            }));
-            return result;
-        } else {
-            return [];
-        }
-    } catch (error) {
-        console.error(error);
-        return [];
+  try {
+    const response = await fetch(`https://gitlab.com/users/${username}/calendar.json`);
+    if (response.status == 200) {
+      const data: Map<string, string> = await response.json();
+      const result = Object.entries(data).map(([date, count]) => ({
+        date,
+        count,
+      }));
+      return result;
+    } else {
+      return [];
     }
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
