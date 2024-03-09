d2f9a480368d76ed93e5f745d34650def08ca681
Stephen Grice <stephengrice@live.com>
2024-03-09 15:39:03 -0500

    Add stubs for each site

--- src/lib/contributions.ts
+++ src/lib/contributions.ts
@@ -4,7 +4,24 @@ interface Contribution {
     count: number
 }
 
-export function getContributions(provider: 'github'|'gitlab', username: string): Contribution[] {
+export async function getContributions(provider: 'github'|'gitlab', username: string): Promise<Contribution[]> {
+    if (provider === 'gitlab') {
+        return await getGitlabContributions(username);
+    } else if (provider === 'github') {
+        return await getGithubContributions(username);
+    }
+}
+
+async function getGithubContributions(username: string): Promise<Contribution[]> {
+    return [
+        {
+            date: '2024-03-09',
+            count: 3,
+        },
+    ];
+}
+
+async function getGitlabContributions(username: string): Promise<Contribution[]> {
     return [
         {
             date: '2024-03-09',
