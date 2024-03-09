eae44bfead2b9190728788828062984633994584
Jake Cyr <cyrjake@gmail.com>
2024-03-09 16:22:32 -0500

    Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,44 @@
-import { Container, Divider } from '@mantine/core';
+import { Container } from '@mantine/core';
 import { useState } from 'react';
 import { Account } from '@/models/Account';
 import { AddAccount } from '@/components/AddAccount';
 import { AccountList } from '@/components/AccountList';
+import Calendar from 'react-github-contribution-calendar';
+import { Title } from '@mantine/core';
 
 export default function Home() {
   const [accounts, setAccounts] = useState<Account[]>([]);
+  const [contributions, setContributions] = useState<Record<string, number>>({});
+  const [lastDate, setLastDate] = useState<string>('');
+
+  const handleAccountAdd = async (newAccount: Account) => {
+    const allAccounts = [...accounts, newAccount];
+    setAccounts(allAccounts);
+
+    const response = await fetch('/api/contributions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(allAccounts),
+    });
+
+    const data = await response.json();
+    setContributions(data);
+    setLastDate(Object.keys(data).sort().reverse()[0]);
+  };
 
   return (
     <Container>
-      <AddAccount onAccountAdd={(account) => setAccounts([...accounts, account])} />
-      <Divider className='mt-2' />
+      <Title order={1}>Code Contribution Heatmap</Title>
+      <AddAccount onAccountAdd={(account) => handleAccountAdd(account)} />
       <AccountList accounts={accounts} />
+
+      {accounts.length > 0 ? (
+        <Calendar values={contributions} until={lastDate} />
+      ) : (
+        <p>No contributions yet</p>
+      )}
     </Container>
   );
 }
