f7ede5c069b0242857e854ba1aaefca5942b13ba
Stephen Grice <stephengrice@live.com>
2024-03-09 13:02:15 -0500

    Get stripe api working

--- api/email_agent_api/routes/user.py
+++ api/email_agent_api/routes/user.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 from email_agent_api.lib.payment import check_if_paid
+from email_agent_api.lib.log import logger
+
 
 router = APIRouter()
 
 
 @router.get("/")
 def get_user_status(email: str) -> dict[str, str]:
+    logger.info('hello wrold')
     if check_if_paid(email):
         return {"status": "paid"}
     else:
