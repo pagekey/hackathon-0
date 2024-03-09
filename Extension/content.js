8b9cb4ac4095d958b9e8212f6c3e732ec0223a49
Monroe Clinton <contact@monroeclinton.com>
2024-03-09 17:18:09 -0500

    Mail parsing

--- Extension/content.js
+++ Extension/content.js
@@ -1,10 +1,21 @@
 console.log('content.js');
 
+const API_URL = 'http://0.0.0.0:8000';
 const gmail = new Gmail();
 
 gmail.observe.on('compose', (compose, type) => {
-    gmail.tools.add_compose_button(compose.$el, 'Generate Response', () => {
+    gmail.tools.add_compose_button(compose.$el, 'Generate Response', async () => {
         var emailDom = gmail.dom.email(gmail.get.email_id());
         const source = emailDom.source();
+
+        const res = await fetch(API_URL + '/api/generate/', {
+            method: 'POST',
+            body: source,
+        });
+
+        const email = await res.json();
+        const body = email.body;
+
+        emailDom.body(body);
     });
 });
--- api/email_agent_api/routes/generate_email.py
+++ api/email_agent_api/routes/generate_email.py
@@ -1,9 +1,8 @@
 from collections.abc import AsyncIterable
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import StreamingResponse
 from email_agent_api.get_llm_instance import get_llm_instance
 from langchain.llms.base import BaseLLM
-from email_agent_api.routes.example_emails import ALL_EMAILS
 from email_agent_api.lib.get_llm_message_stream import get_llm_message_stream
 
 router = APIRouter()
@@ -12,20 +11,23 @@ llm_instance: BaseLLM = get_llm_instance()
 EMAIL_GENERATION_SYSTEM_PROMPT = """You you are an email generation service acting as {user_email}. Given a list of previous
 emails above in the thread along with the name of the person you are acting as, craft
 a response email in the same tone as sender's previous emails. You should sound like you are {user_email}
-Respond with only the email content and nothing else."""
+Respond with only the email body and nothing else."""
 
 MOCK_USER_EMAIL = "steve.grice@pagekey.com"
 
 
 @router.post("/")
-async def generate_email() -> dict[str, str]:
-    all_emails: str = "\n\n".join(ALL_EMAILS)
+async def generate_email(request: Request) -> dict[str, str]:
+    all_emails = await request.body()
+
     formatted_system_prompt: str = EMAIL_GENERATION_SYSTEM_PROMPT.format(
         user_email=MOCK_USER_EMAIL
     )
     full_prompt: str = f"----Email Thread-----{all_emails}\n-----End Email Thread-----\n\n{formatted_system_prompt}"
-    response: str = llm_instance.invoke(full_prompt)
-    return {"email": response}
+    response: str = llm_instance.invoke(full_prompt, max_tokens=16385)
+    return {
+        "body": response
+    }
 
 
 @router.post("/stream")
