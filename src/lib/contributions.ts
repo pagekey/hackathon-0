b5cd1ee99e16a7f650972cec0dff94be6d2297c8
Stephen Grice <stephengrice@live.com>
2024-03-09 16:58:25 -0500

    Get github contributions to pull

--- src/lib/contributions.ts
+++ src/lib/contributions.ts
@@ -1,5 +1,6 @@
 import { Account } from '@/models/Account';
 import { Contribution } from '@/models/Contribution';
+import { parseFromString } from 'dom-parser';
 
 export async function getContributions(account: Account): Promise<Contribution[]> {
   if (account.provider === 'gitlab') {
@@ -12,12 +13,33 @@ export async function getContributions(account: Account): Promise<Contribution[]
 }
 
 async function getGithubContributions(username: string): Promise<Contribution[]> {
-  return [
-    {
-      date: '2024-03-09',
-      count: 3,
-    },
-  ];
+    try {
+        const response = await fetch(`https://github.com/${username}`);
+        if (response.status == 200) {
+            // Parse DOM returned in HTML response
+            const dom = parseFromString(await response.text());
+            const contributions = dom.getElementsByClassName('ContributionCalendar-day');
+            const result =  contributions.map(contribution => {
+                const date = contribution.getAttribute('data-date');
+                const tooltips = dom.getElementsByAttribute('for', contribution.getAttribute('id'));
+                if (tooltips[0] && tooltips[0].innerHTML) {
+                    const count = parseInt(tooltips[0].innerHTML.split(' ')[0]) || 0;
+                    const result: Contribution = { date, count };
+                    console.log('got small result',result)
+                    return result;
+                } else {
+                    return { date, count: 0 };
+                }
+            }).filter(contribution => contribution.count > 0);
+            console.log('got big result',result)
+            return result;
+        } else {
+            return [];
+        }
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
 
 async function getGitlabContributions(username: string): Promise<Contribution[]> {
