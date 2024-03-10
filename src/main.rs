e606b435ad02d35d1bcb853ad1a947692e717665
Stephen Grice <stephengrice@live.com>
2024-03-09 22:02:59 -0500

    Print stuff every two seconds now

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ struct Name(String);
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
 }
 
@@ -19,19 +18,38 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Danny".to_string())));
 }
 
-fn hello_world() {
-    println!("Hello world!");
-}
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>, 
+    query: Query<&Name, With<Person>>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
+
+
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
     for mut name in &mut query {
         if name.0 == "Elaina" {
             name.0 = "Yeetmeister".to_string();
             break;
         }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
     }
 }
