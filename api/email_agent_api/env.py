ba0c0993e8b4254c3dc02a53fa1c9eb8ce00e961
Monroe Clinton <contact@monroeclinton.com>
2024-03-10 00:24:22 -0500

    CHROME_EXTENSION_ID environment variable

--- api/email_agent_api/env.py
+++ api/email_agent_api/env.py
@@ -9,3 +9,4 @@ LLM_MODEL_NAME: str = os.environ.get("LLM_MODEL_NAME", "gpt-3.5-turbo-0125")
 STRIPE_SECRET_KEY: str = os.environ["STRIPE_SECRET_KEY"]
 STRIPE_PRICE_ID: str = os.environ['STRIPE_PRICE_ID']
 STRIPE_CHECKOUT_URL: str = os.environ['STRIPE_CHECKOUT_URL']
+CHROME_EXTENSION_ID: str = os.environ['CHROME_EXTENSION_ID']
