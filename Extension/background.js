a7b55cdbbc86992978cf93149fcc0802973bc575
Monroe Clinton <contact@monroeclinton.com>
2024-03-10 00:22:05 -0500

    Background worker for checking purchases

--- Extension/background.js
+++ Extension/background.js
@@ -1,88 +1,25 @@
-//oauth2 auth
-chrome.identity.getAuthToken(
-	{'interactive': true},
-	function(){
-	  //load Google's javascript client libraries
-		window.gapi_onload = authorize;
-		loadScript('https://apis.google.com/js/client.js');
-	}
-);
+const API_URL = 'http://0.0.0.0:8000';
 
-function loadScript(url){
-  var request = new XMLHttpRequest();
+const hasPurchased = async (sendResponse) => {
+  try {
+    const auth = await chrome.identity.getAuthToken({
+      interactive: true
+    });
 
-	request.onreadystatechange = function(){
-		if(request.readyState !== 4) {
-			return;
-		}
+    const res = await fetch(API_URL + '/api/user/?token=' + auth.token);
+    const status = await res.json();
 
-		if(request.status !== 200){
-			return;
-		}
+    sendResponse(status.active);
+  } catch (e) {
+    sendResponse(false);
+    return;
+  }
+};
 
-    eval(request.responseText);
-	};
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "hasPurchased") {
+	hasPurchased(sendResponse);
+  }
 
-	request.open('GET', url);
-	request.send();
-}
-
-function authorize(){
-  gapi.auth.authorize(
-		{
-			client_id: '336332834621-6mku9gauqh3l6j0j472iimjdumsnrglj.apps.googleusercontent.com',
-			immediate: true,
-			scope: 'https://www.googleapis.com/auth/gmail.modify'
-		},
-		function(){
-		  gapi.client.load('gmail', 'v1', gmailAPILoaded);
-		}
-	);
-}
-
-function gmailAPILoaded(){
-    //do stuff here
-}
-
-
-/* here are some utility functions for making common gmail requests */
-function getThreads(query, labels){
-  return gapi.client.gmail.users.threads.list({
-		userId: 'me',
-		q: query, //optional query
-		labelIds: labels //optional labels
-	}); //returns a promise
-}
-
-//takes in an array of threads from the getThreads response
-function getThreadDetails(threads){
-  var batch = new gapi.client.newBatch();
-
-	for(var ii=0; ii<threads.length; ii++){
-		batch.add(gapi.client.gmail.users.threads.get({
-			userId: 'me',
-			id: threads[ii].id
-		}));
-	}
-
-	return batch;
-}
-
-function getThreadHTML(threadDetails){
-  var body = threadDetails.result.messages[0].payload.parts[1].body.data;
-	return B64.decode(body);
-}
-
-function archiveThread(id){
-  var request = gapi.client.request(
-		{
-			path: '/gmail/v1/users/me/threads/' + id + '/modify',
-			method: 'POST',
-			body: {
-				removeLabelIds: ['INBOX']
-			}
-		}
-	);
-
-	request.execute();
-}
+  return true;
+});
